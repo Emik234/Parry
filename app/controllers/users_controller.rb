c7d06609d6c0de606ceb9711cfdd637b05aa28ab
William Galarza <w.galarza234@gmail.com>
2015-03-26 14:54:29 -0400

    Modified the controllers for users and styled a bit of bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
   def index
+    @users = User.all
   end
 
   def edit
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
@@ -13,5 +16,34 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user.destroy
+    redirect_to @user, notice: "User deleted"
+  end
+
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        redirect_to @user, notice: "User successfully created"
+      else
+        render :new
+  end
+end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "User successfully updated"
+    else
+      render :edit
+  end
+end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:fname, :alias, :fid, :ffc)
   end
 end
